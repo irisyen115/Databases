5d5ca203148636e3fc606efc0eb7d1da58dc68bb
irisyen115 <98208602+irisyen115@users.noreply.github.com>
2023-12-11 10:26:46 +0800

    finish lab09 HW

---
+++ Lab09/9-5-2-1.sql
@@ -0,0 +1,5 @@
+Create Table Teacher(
+T_id char(5),
+T_name nvarchar(10) Not Null,
+Primary Key(T_id)
+) 
---
+++ Lab09/9-5-2-2.sql
@@ -0,0 +1,6 @@
+Create Table Course( 
+c_id char(5), 
+c_name  nvarchar(10) Not Null, 
+T_id char(5) 
+Primary Key(C_id)
+)
